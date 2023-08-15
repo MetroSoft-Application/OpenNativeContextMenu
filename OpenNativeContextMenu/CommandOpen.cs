54198cf888ef7a724ba278e730554bb18592adb9
alexisonwire <adtd1989@gmail.com>
2023-08-15 19:00:39 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenNativeContextMenu/CommandOpen.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Globalization;
+
+namespace OpenNativeContextMenu
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CommandOpen
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("fff3af42-25a1-4fdb-9142-b6e0c21ab32c");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandOpen"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CommandOpen(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CommandOpen Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CommandOpen's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CommandOpen(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            try
+            {
+                //DTEを取得し、nullチェック
+                var dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
+                Assumes.Present(dte);
+
+                //選択ファイル取得
+                var solutionExplorer = dte.ToolWindows.SolutionExplorer;
+                var selectedItems = (Array)solutionExplorer.SelectedItems;
+                if (selectedItems.Length != 1)
+                {
+                    return;
+                }
+                var firstSelectedItem = selectedItems.GetValue(0) as UIHierarchyItem;
+                var projectItem = firstSelectedItem?.Object as ProjectItem;
+                //選択ファイルのパスを取得
+                var path = projectItem?.FileNames[1];
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    OpenContexMenu(path);
+                    return;
+                }
+
+                var project = firstSelectedItem?.Object as Project;
+                //選択プロジェクトファイルのパスを取得
+                path = project?.FullName;
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    OpenContexMenu(path);
+                    return;
+                }
+
+                var solution = firstSelectedItem?.Object as Solution;
+                //選択ソリューションファイルのパスを取得
+                path = solution?.FullName;
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    OpenContexMenu(path);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                ActivityLog.LogError("OpenContextMenu", ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// コンテキストメニューを開く
+        /// </summary>
+        /// <param name="path">開く対象のファイルパス</param>
+        private void OpenContexMenu(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+            const string exeName = "OpenContextMenu.exe";
+            var baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var targetDirectory = Path.Combine(baseDir, @"Exec");
+            var exe = Directory.GetFiles(targetDirectory, exeName, SearchOption.AllDirectories).First();
+            if (!string.IsNullOrWhiteSpace(exe))
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = exe,
+                    Arguments = path
+                };
+                using (var process = System.Diagnostics.Process.Start(startInfo))
+                {
+                    process.WaitForExit();
+                }
+            }
+        }
+    }
+}
---
+++ OpenPropertiesDialog/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace OpenPropertiesDialog
+{
+    class OpenPropertiesDialog
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
+        internal struct SHELLEXECUTEINFO
+        {
+            public int cbSize;
+            public int fMask;
+            public IntPtr hWnd;
+            [MarshalAs(UnmanagedType.LPTStr)] public string lpVerb;
+            [MarshalAs(UnmanagedType.LPTStr)] public string lpFile;
+            [MarshalAs(UnmanagedType.LPTStr)] public string lpParameters;
+            [MarshalAs(UnmanagedType.LPTStr)] public string lpDirectory;
+            public int nShow;
+            public IntPtr hInstApp;
+            public IntPtr lpIDList;
+            [MarshalAs(UnmanagedType.LPTStr)] public string lpClass;
+            public IntPtr hkeyClass;
+            public int dwHotKey;
+            public IntPtr hIcon;
+            public IntPtr hProcess;
+        }
+
+        internal const int SEE_MASK_INVOKEIDLIST = 0x000C;
+        internal const int SEE_MASK_NOCLOSEPROCESS = 0x0040;
+        internal const int SEE_MASK_FLAG_NO_UI = 0x0400;
+
+        [DllImport("shell32.dll", EntryPoint = "ShellExecuteEx", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal extern static bool ShellExecuteEx(ref SHELLEXECUTEINFO sei);
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        private const string DIALOG_CLASS_NAME = "#32770";
+
+        private static string GetWindowClassName(IntPtr hWnd)
+        {
+            StringBuilder className = new StringBuilder(256);
+            GetClassName(hWnd, className, className.Capacity);
+            return className.ToString();
+        }
+
+        private static List<IntPtr> FindWindowsWithTitleContaining(string targetTitlePart)
+        {
+            var windowHandles = new List<IntPtr>();
+
+            EnumWindows((hWnd, lParam) =>
+            {
+                var length = GetWindowTextLength(hWnd);
+                if (length == 0)
+                {
+                    return true;
+                }
+
+                var builder = new StringBuilder(length);
+                GetWindowText(hWnd, builder, length + 1);
+                var className = GetWindowClassName(hWnd);
+                if (builder.ToString().Contains(targetTitlePart) && className == DIALOG_CLASS_NAME)
+                {
+                    windowHandles.Add(hWnd);
+                }
+
+                return true;
+            }, IntPtr.Zero);
+
+            return windowHandles;
+        }
+
+        private static string GetFileNameOrDirectoryName(string path)
+        {
+            return Path.GetFileName(path) ?? new DirectoryInfo(path).Name;
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            SHELLEXECUTEINFO info;
+            info = new SHELLEXECUTEINFO();
+            info.cbSize = Marshal.SizeOf(info);
+            info.fMask = SEE_MASK_NOCLOSEPROCESS | SEE_MASK_INVOKEIDLIST | SEE_MASK_FLAG_NO_UI;
+            info.hWnd = IntPtr.Zero;
+            info.lpFile = args[0];
+            info.lpVerb = "properties";
+            info.lpParameters = "\0";
+            info.lpDirectory = "\0";
+            info.nShow = 0;
+            info.hInstApp = IntPtr.Zero;
+            info.lpIDList = IntPtr.Zero;
+            info.lpClass = "\0";
+            info.hkeyClass = IntPtr.Zero;
+            info.dwHotKey = 0;
+            info.hIcon = IntPtr.Zero;
+            info.hProcess = IntPtr.Zero;
+
+            if (!ShellExecuteEx(ref info))
+            {
+                Console.WriteLine("Failed");
+                return;
+            };
+
+            // 表示されるまで待つ
+            var hdlList = new List<IntPtr>();
+            var path = GetFileNameOrDirectoryName(args[0]);
+            while (true)
+            {
+                hdlList = FindWindowsWithTitleContaining(path);
+                if (hdlList.Count > 0)
+                {
+                    break;
+                }
+            }
+
+            var anyWindowClosed = false;
+            while (true)
+            {
+                foreach (var handle in hdlList)
+                {
+                    if (!IsWindow(handle))
+                    {
+                        anyWindowClosed = true;
+                        break;
+                    }
+                }
+
+                if (anyWindowClosed)
+                {
+                    break;
+                }
+                // 1秒ごとにチェック
+                Thread.Sleep(1000);
+            }
+            CloseHandle(info.hProcess);
+        }
+    }
+}
